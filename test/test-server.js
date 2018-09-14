590f3441677149e4502962499b8cb18ab70082bc
Cameron Hatch <cameronlavigne92@gmail.com>
2018-09-14 11:23:04 -0500

    added tests for recipes

--- test/test-server.js
+++ test/test-server.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const chai = require("chai");
 const chaiHttp = require("chai-http");
 
@@ -149,4 +151,109 @@ describe("Shopping List", function() {
         })
     );
   });
+});
+
+describe('Recipes', function(){
+  before(function() {
+    return runServer();
+  });
+
+  after(function() {
+    return closeServer();
+  });
+
+  it('should return list of recipes on GET',  function(){
+    return chai
+      .request(app)
+      .get('/recipes')
+      .then(function(res){
+        expect(res).to.have.status(200);
+        expect(res).to.be.json;
+        expect(res.body).to.be.a("array");
+
+        // because we create three items on app load
+        expect(res.body.length).to.be.at.least(1);
+        // each item should be an object with key/value pairs
+        // for `id`, `name` and `checked`.
+        const expectedKeys = ["id", "name", "ingredients"];
+        res.body.forEach(function(item) {
+          expect(item).to.be.a("object");
+          expect(item).to.include.keys(expectedKeys);
+        });
+      });
+  });
+
+  it("should add an item on POST", function() {
+    const newItem = { name: "cake", ingredients: ['flour', 'sugar'] };
+    return chai
+      .request(app)
+      .post("/recipes")
+      .send(newItem)
+      .then(function(res) {
+        expect(res).to.have.status(201);
+        expect(res).to.be.json;
+        expect(res.body).to.be.a("object");
+        expect(res.body).to.include.keys("id", "name", "ingredients");
+        expect(res.body.id).to.not.equal(null);
+        // response should be deep equal to `newItem` from above if we assign
+        // `id` to it from `res.body.id`
+        expect(res.body).to.deep.equal(
+          Object.assign(newItem, { id: res.body.id })
+        );
+      });
+  });
+
+  it("should update items on PUT", function() {
+    // we initialize our updateData here and then after the initial
+    // request to the app, we update it with an `id` property so
+    // we can make a second, PUT call to the app.
+    const updateData = {
+      name: "rice",
+      ingredients: ['rice', 'water']
+    };
+
+    return (
+      chai
+        .request(app)
+        // first have to get so we have an idea of object to update
+        .get("/recipes")
+        .then(function(res) {
+          updateData.id = res.body[0].id;
+          // this will return a promise whose value will be the response
+          // object, which we can inspect in the next `then` block. Note
+          // that we could have used a nested callback here instead of
+          // returning a promise and chaining with `then`, but we find
+          // this approach cleaner and easier to read and reason about.
+          return chai
+            .request(app)
+            .put(`/recipes/${updateData.id}`)
+            .send(updateData);
+        })
+        // prove that the PUT request has right status code
+        .then(function(res) {
+          expect(res).to.have.status(204);
+        })
+    );
+  });
+
+  it("should add an item on POST", function() {
+    const newItem = { name: "coffee", ingredients: ['water', 'coffee'] };
+    return chai
+      .request(app)
+      .post("/recipes")
+      .send(newItem)
+      .then(function(res) {
+        expect(res).to.have.status(201);
+        expect(res).to.be.json;
+        expect(res.body).to.be.a("object");
+        expect(res.body).to.include.keys("id", "name", "ingredients");
+        expect(res.body.id).to.not.equal(null);
+        // response should be deep equal to `newItem` from above if we assign
+        // `id` to it from `res.body.id`
+        expect(res.body).to.deep.equal(
+          Object.assign(newItem, { id: res.body.id })
+        );
+      });
+  });
+
 });
